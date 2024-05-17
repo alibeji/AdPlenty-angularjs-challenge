08ece4fae2d71dc028ad6ee0615ed40e3af9853c
alibeji98 <alibeji98@gmail.com>
2024-05-17 20:27:05 +0200

    feat: add api call and image grid

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,14 +1,39 @@
-'use strict';
+"use strict";
 
-angular.module('myApp.view1', ['ngRoute'])
+angular
+  .module("myApp.view1", ["ngRoute"])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
-}])
+  .config([
+    "$routeProvider",
+    function ($routeProvider) {
+      $routeProvider.when("/view1", {
+        templateUrl: "view1/view1.html",
+        styleUrls: ['view1/view1.css'],
+        controller: "View1Ctrl",
+      });
+    },
+  ])
+  .controller("View1Ctrl", [
+    "$http",
+    function ($http) {
+      let vm=this
+      vm.images=null
 
-.controller('View1Ctrl', [function() {
-
-}]);
+      $http({
+        method: "GET",
+        url: "https://api.unsplash.com/search/photos",
+        params: {
+          query: "green",
+          page: 1,
+          per_page: 9,
+          client_id: "mc022uV3PnBEenyHqnvPyCbvybr9q1FohSeLtqly80Q",
+        },
+      })
+        .then(function (response) {
+          vm.images=response.data
+        })
+        .catch(function (error) {
+          console.error("Error:", error);
+        });
+    },
+  ]);
