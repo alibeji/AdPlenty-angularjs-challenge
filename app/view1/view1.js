1c64b776c9032a3bda4cefe6ef0b7f8a5e2fc139
alibeji98 <alibeji98@gmail.com>
2024-05-17 22:38:13 +0200

    style: prettify files

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,71 +1,72 @@
-"use strict";
+'use strict';
 
 angular
-  .module("myApp.view1", ["ngRoute"])
+  .module('myApp.view1', ['ngRoute'])
 
   .config([
-    "$routeProvider",
+    '$routeProvider',
     function ($routeProvider) {
-      $routeProvider.when("/view1", {
-        templateUrl: "view1/view1.html",
+      $routeProvider.when('/view1', {
+        templateUrl: 'view1/view1.html',
         styleUrls: ['view1/view1.css'],
-        controller: "View1Ctrl",
+        controller: 'View1Ctrl',
       });
     },
   ])
-  .controller("View1Ctrl", [
-    "$http", "$scope",
-    function($http) { 
+  .controller('View1Ctrl', [
+    '$http',
+    '$scope',
+    function ($http) {
       const vm = this;
       vm.images = null;
-      vm.searchTerm = "green";
-      vm.currentPage = 1
-      vm.isLoading = false
+      vm.searchTerm = 'landscape';
+      vm.currentPage = 1;
+      vm.isLoading = false;
 
       const searchImages = () => {
-        vm.isLoading = true
+        vm.isLoading = true;
         $http({
-          method: "GET",
-          url: "https://api.unsplash.com/search/photos",
+          method: 'GET',
+          url: 'https://api.unsplash.com/search/photos',
           params: {
             query: vm.searchTerm,
             page: vm.currentPage,
-            per_page: 9,
-            client_id: "mc022uV3PnBEenyHqnvPyCbvybr9q1FohSeLtqly80Q",
+            per_page: 8,
+            client_id: 'mc022uV3PnBEenyHqnvPyCbvybr9q1FohSeLtqly80Q',
           },
           headers: {
-            Authorization: "Client-ID mc022uV3PnBEenyHqnvPyCbvybr9q1FohSeLtqly80Q"
-
-          }
+            Authorization:
+              'Client-ID mc022uV3PnBEenyHqnvPyCbvybr9q1FohSeLtqly80Q',
+          },
         })
-          .then(function(response) {
+          .then(function (response) {
             vm.images = response.data;
-            vm.isLoading = false
+            vm.isLoading = false;
+            console.log(response.data);
           })
-          .catch(function(error) {
-            console.error("Error:", error);
+          .catch(function (error) {
+            console.error('Error:', error);
             vm.isLoading = false;
           });
-      }
+      };
 
       searchImages();
 
       vm.searchImages = () => {
-        vm.currentPage = 1
-        searchImages()
-      }
+        vm.currentPage = 1;
+        searchImages();
+      };
       vm.changePage = (direction) => {
-        vm.currentPage = direction === 'next' ? vm.currentPage+1 : vm.currentPage-1
-        searchImages()
-      }
+        vm.currentPage =
+          direction === 'next' ? vm.currentPage + 1 : vm.currentPage - 1;
+        searchImages();
+      };
 
-      vm.searchOnEnter = function($event) {
+      vm.searchOnEnter = function ($event) {
         if ($event.keyCode === 13) {
-          vm.currentPage = 1
+          vm.currentPage = 1;
           searchImages();
         }
       };
-
-      
-    }
+    },
   ]);
